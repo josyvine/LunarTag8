fd76113822cb51e7b225cd7c1920bc52e466055b
josyvine <64621103+josyvine@users.noreply.github.com>
2025-11-20 22:47:55 +0530

    Update ImageViewerActivity.java

--- app/src/main/java/com/lunartag/app/ui/viewer/ImageViewerActivity.java
+++ app/src/main/java/com/lunartag/app/ui/viewer/ImageViewerActivity.java
@@ -1,6 +1,6 @@
 package com.lunartag.app.ui.viewer;
 
-import android.app.AlertDialog;
+import android.app.AlertDialog; 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
